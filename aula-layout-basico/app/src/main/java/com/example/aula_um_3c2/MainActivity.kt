dc23430a1ad505e70565fbac3178133a9deade75
Mafe <contato.mf.13@gmail.com>
2023-03-14 10:00:00 -0300

    Add basic Android layout activity

---
+++ aula-layout-basico/app/src/main/java/com/example/aula_um_3c2/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.aula_um_3c2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
